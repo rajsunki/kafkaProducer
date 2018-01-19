0b91a17b9c5a0c006ac0b456d6762bedd305a06d
rajsunki <rajsunki@Raj.localdomain>
2018-01-19 16:53:03 +0530

    kafkaProducer

---
+++ kafka.js
@@ -0,0 +1,17 @@
+var kafka = require('kafka-node'),
+    Producer = kafka.Producer,
+    KeyedMessage = kafka.KeyedMessage,
+    client = new kafka.Client("localhost:2181"),
+    producer = new Producer(client),
+    km = new KeyedMessage('key', 'some message to broker'),
+    payloads = [
+        { topic: 'topic_name', messages: ['hello', 'world', km] }
+    ];
+producer.on('ready', function () {
+    producer.send(payloads, function (err, data) {
+        console.log(data);
+    });
+});
+ 
+producer.on('error', function (err) {})
+console.log("HI");
